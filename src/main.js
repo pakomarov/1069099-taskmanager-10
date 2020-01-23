12fe91d44ae4ef0b9a1cdd34c2ab913bd673ddf3
Pavel Komarov <komarov.pa.it@gmail.com>
2020-01-23 22:27:33 +0300

    Выносит в отдельный компонент сортировку доски с задачами

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import {createTemplateElement, render} from './utils.js';
-import {createSiteMenuTemplate} from './components/site-menu.js';
 import {TASK_COUNT} from './const.js';
+import {createSiteMenuTemplate} from './components/site-menu.js';
 import {createFilterTemplate} from './components/filter.js';
 import {createBoardTemplate} from './components/board.js';
+import {createSortingTemplate} from './components/sorting.js';
 import {createTaskTemplate} from './components/task.js';
 import {createTaskEditTemplate} from './components/task-edit.js';
 import {createLoadMoreButtonTemplate} from './components/load-more-button.js';
@@ -21,14 +22,15 @@ const renderSiteComponents = () => {
   const boardTemplateElement = createTemplateElement(createBoardTemplate());
   fragment.appendChild(boardTemplateElement.content);
 
-  const taskListElement = fragment.querySelector(`.board__tasks`);
+  const boardElement = fragment.querySelector(`.board`);
+  render(boardElement, createSortingTemplate(), `afterbegin`);
+
+  const taskListElement = boardElement.querySelector(`.board__tasks`);
   render(taskListElement, createTaskEditTemplate(), `beforeend`);
 
   for (let i = 0; i < TASK_COUNT; i++) {
     render(taskListElement, createTaskTemplate(), `beforeend`);
   }
-
-  const boardElement = fragment.querySelector(`.board`);
 
   render(boardElement, createLoadMoreButtonTemplate(), `beforeend`);
 
